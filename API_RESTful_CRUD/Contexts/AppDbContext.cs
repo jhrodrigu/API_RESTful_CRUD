c97e34abdf88586cbba4ad6cbdd7764b95a8b9a8
Yonny Rodriguez <jhrodrigu@hotmail.com>
2020-05-23 03:15:29 -0500

    Agregar archivos de proyecto.

---
+++ API_RESTful_CRUD/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_RESTful_CRUD.Contexts
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ API_RESTful_CRUD/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API_RESTful_CRUD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GET(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        //DELETE api/values/5
+        [HttpPut("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+
+    }
+}
